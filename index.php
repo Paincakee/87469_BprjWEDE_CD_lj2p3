d98d07ea0b22cadcece21f73c41c1d89fcc96b91
Richard Vullings <rvullings8@gmail.com>
2023-04-18 13:48:51 +0200

    css working

--- index.php
+++ index.php
@@ -18,28 +18,48 @@ session_start();
     </div>
     <div class="game_wrapper">
         <div class='description_wrapper'>
-        <?php
-        shuffle($_SESSION['dataDescription']);
-        shuffle($_SESSION['dataImages']);
+            <?php
+            // Shuffle the arrays
+            shuffle($_SESSION['dataDescription']);
+            shuffle($_SESSION['dataImages']);
 
-        foreach ($_SESSION['dataDescription'] as $description) { 
+            $matchingPairs = array();
+            foreach ($_SESSION['dataDescription'] as $description) {
+                foreach ($_SESSION['dataImages'] as $img) {
+                    if ($description['matchKey'] == $img['matchKey']) {
+                        $matchingPairs[] = array('description' => $description, 'img' => $img);
+                        break;
+                    }
+                }
+                if (count($matchingPairs) >= 5) {
+                    break;
+                }
+            }
+
+            foreach ($matchingPairs as $pair) { 
+                ?>
+                    <div onclick='flipCard(event);' class='descriptions click_box' data-match-id="<?= $pair['description']['matchKey'] ?>" ><?=$pair['description']['description_img']?></div>
+                <?php 
+            }
             ?>
-                <div onclick='flipCard(event);' class='descriptions click_box' data-match-id="<?= $description['matchKey'] ?>" ><?= $description['description_img'] ?></div>
-            <?php 
-        }
-        ?>
         </div>
-
+        <div class="info">
+            <span id="timer_text">30 Seconds</span>
+            <div class="leaderboard">Hallp</div>
+        </div>
+           
         <div class="image_wrapper">
-        <?php
-        foreach ($_SESSION['dataImages'] as $img) { 
+            <?php
+            shuffle($matchingPairs);
+            foreach ($matchingPairs as $pair) { 
+                ?>
+                <img onclick="flipCard(event);" src="img/<?= $pair['img']['name_img'] ?>" alt="<?= basename($pair['img']['name_img']) ?>" data-match-id="<?= $pair['img']['matchKey'] ?>" width="200px" height="150px" class="images click_box">
+                <?php 
+            }
             ?>
-                <img onclick="flipCard(event);" src="<?= $img['name_img'] ?>" alt="<?= basename($img['name_img']) ?>" data-match-id="<?= $img['matchKey'] ?>" width="200px" height="150px" class="click_box">
-            <?php 
-        }
-        ?>
         </div>
     </div>
+
 
    
 </body>
--- js/match.js
+++ js/match.js
@@ -1,61 +1,92 @@
 let timeoutID;
+let timerStarted = false;
+let timerIntervalId;
+let timer = 0;
+let user = "";
 
 function flipCard(event) {
     // Get the number of active elements and the clicked card element
     const activeElements = document.querySelectorAll('.active').length;
     const clickedCard = event.currentTarget.closest('.click_box');
 
+    
     // Ignore clicks on already matched cards or if there are already 2 active cards
     if (clickedCard.classList.contains('won') || activeElements >= 2) {
         return;
     }
 
+    const activeDescs = document.querySelectorAll('.descriptions.active').length;
+    const activeImages = document.querySelectorAll('.images.active').length;
+
+    // Ignore clicks on descriptions if there is already one or more active descriptions
+    if (clickedCard.classList.contains('descriptions') && activeDescs > 0 || clickedCard.classList.contains('images') && activeImages > 0) {
+        return;
+    }
+
+    if (!timerStarted) {
+        timerStarted = true;
+        const startTime = Date.now() + 30000; // set the start time to 30 seconds from now
+        let styleTime = 30
+        timerIntervalId = setInterval(() => {
+            const won = document.querySelectorAll('.won').length;
+            timer = (startTime - Date.now()) / 1000; // calculate the remaining time
+            document.getElementById("timer_text").innerHTML = (timer <= 0 ? "0.00" : timer.toFixed(2)) + " seconds";
+
+            // Stop the timer when all cards are matched or time is up
+            if (won == 10 || timer <= 0 ) {
+                clearInterval(timerIntervalId);
+                let text;
+                if (won == 10) {
+                    setTimeout(() => {
+                        const message = `You have found all pairs. Time left: ${timer.toFixed(2)} seconds \nWhat is your name?`;
+                        user = prompt(message, "");
+                    }, 10);
+                } else {
+                    
+                    setTimeout(() => {
+                        alert(Math.max(0, timer).toFixed(2));
+                    }, 10);
+                      
+                    
+                }
+
+                // Save stats and reset game when user enters their name
+                if (user != null) {
+                    
+                } else if(user = ""){
+                    alert('Stats not saved');
+                    location.reload();
+                }
+            }
+        }, 10);
+    }
+    
     if (activeElements < 2) {
+        
         if (!clickedCard.classList.contains('active')) {
             clickedCard.classList.add('active');
         }
 
-        // Set timeout to check for a match after a short delay
-        clearTimeout(timeoutID);
-        timeoutID = setTimeout(checkForMatch, 430);
-    } else if (activeElements == 2) {
-        // Get the matchIds of the two flipped cards
-        const flippedCards = document.querySelectorAll('.active');
-        const matchId1 = flippedCards[1].getAttribute('data-match-id');
-        const matchId2 = flippedCards[flippedCards.length - 2].getAttribute('data-match-id');
 
-        console.log(matchId1);
-        console.log(matchId2);
-        
-        // If the cards match, mark them as won
-        if (matchId1 == matchId2) {
-            toggleClasses(flippedCards[1], flippedCards[flippedCards.length - 2], "active", "won"); 
-        } else {
-            // If the cards don't match, flip them back over
-            if (!flippedCards[1].classList.contains('won') && !flippedCards[flippedCards.length - 2].classList.contains('won')) {
-                flippedCards[1].classList.remove("active");
-                flippedCards[flippedCards.length - 2].classList.remove("active");
-            }
-        }
-    }
+        clearTimeout(timeoutID);
+        timeoutID = setTimeout(checkForMatch, 400);
+
+    } 
 }
 
-function checkForMatch() {
+function checkForMatch(event) {
     const flippedCards = document.querySelectorAll('.active');
         
-  
-    if (flippedCards.length === 2) {
+    if (flippedCards.length == 2) {
         const card1Data = flippedCards[1].getAttribute('data-match-id');
         const card2Data = flippedCards[flippedCards.length - 2].getAttribute('data-match-id');
 
         const card1 = flippedCards[1];
         const card2= flippedCards[flippedCards.length - 2];
         
-        console.log(card1Data);
-        console.log(card2Data);
-        if (card1Data === card2Data) {
+        if (card1Data == card2Data) {
 
-            console.log("ez game");
+            // console.log("Found");
             card1.classList.remove("active");
             card2.classList.remove("active");
 
